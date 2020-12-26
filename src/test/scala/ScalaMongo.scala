4631e675a9da29c72a8b2d8d3800ea636494bb7d
silenceofranger <38451055+silenceofranger@users.noreply.github.com>
2020-12-26 23:30:12 +0530

    Initial commit

---
+++ src/test/scala/ScalaMongo.scala
@@ -0,0 +1,36 @@
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import org.mongodb.scala._
+
+object ScalaMongo extends App {
+
+  implicit class DocumentObservable[C](val observable: Observable[Document]) extends ImplicitObservable[Document] {
+    override val converter: (Document) => String = (doc) => doc.toJson
+  }
+
+  implicit class GenericObservable[C](val observable: Observable[C]) extends ImplicitObservable[C] {
+    override val converter: (C) => String = (doc) => doc.toString
+  }
+
+  trait ImplicitObservable[C] {
+    val observable: Observable[C]
+    val converter: (C) => String
+
+    def results(): Seq[C] = Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
+    def headResult() = Await.result(observable.head(), Duration(10, TimeUnit.SECONDS))
+    def printResults(initial: String = ""): Unit = {
+      if (initial.length > 0) print(initial)
+      results().foreach(res => println(converter(res)))
+    }
+    def printHeadResult(initial: String = ""): Unit = println(s"${initial}${converter(headResult())}")
+  }
+
+  val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017/")
+  val database: MongoDatabase = mongoClient.getDatabase("test")
+  val collection: MongoCollection[Document] =database.getCollection("jobs");
+  val doc: Document = Document("_id" -> 0, "name" -> "MongoDB", "type" -> "database","count" -> 1, "info" -> Document("x" -> 203, "y"-> 102))
+
+  collection.deleteOne(doc).results();    //Using the results() implicit we block until the observer is completed
+
+}
